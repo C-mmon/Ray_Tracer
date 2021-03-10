ad9c90c7c16024b5303e504836e09ca4d8f8c640
C-mmon <44610528+C-mmon@users.noreply.github.com>
2021-03-10 19:50:49 +0530

    Create skeleton.cpp

---
+++ skeleton.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+const int image_width=256;
+const int image_height=256;
+using namespace std;
+int main()
+{
+  cout<<"P3"<<endl;
+  cout<<image_width<<" "<<image_height<<endl;
+  for(int j=image_height-1;j>=0;--j)
+  {
+    for(int i=0;i<image_width;i++)
+    {
+      auto r=double(i)/(image_width-1);
+      auto r=double(i)/(image_width-1);
+      auto b=0.25;
+
+      int ir=static_cast<int>(255)
+      cout<<ir<<' '<<ig<<' '<<ib<<'\n';
+    }
+  }
+  return 0;
+}
